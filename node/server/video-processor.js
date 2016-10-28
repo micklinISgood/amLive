0a59ee1fc548a52c18bcfb2dfa1b88d7f1994419
Mick Lin <cl3469@columbia.edu>
2016-10-27 20:28:04 -0400

    local test done

---
+++ node/server/video-processor.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+var uuid = require('node-uuid');
+var videoFileExtension = '.webm';
+var blobs = [];
+function writeOrAppendData(data, fileName, ws) {
+    var filePath = './uploads/';
+    if (!fs.existsSync(filePath + fileName + videoFileExtension)) {
+        console.log('writing original file');
+        ws.send(fileName);
+        fs.writeFileSync(filePath + fileName + videoFileExtension, data);
+    } else {
+        console.log('appending File')
+        fs.appendFileSync(filePath + fileName + videoFileExtension, data);
+    }
+}
+
+module.exports = function (app) {
+    app.ws('/', function (ws, req) {
+        var fileName = uuid.v1();
+        console.log('new connection established');
+        ws.on('message', function(data) {
+            if (data instanceof Buffer) {
+                writeOrAppendData(data, fileName, ws);
+            }
+        });
+        ws.send(fileName);
+    });
+};
