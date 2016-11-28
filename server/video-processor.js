23e5b715058e143fab8db6f92a015e99e23d40c0
Mick Lin <cl3469@columbia.edu>
2016-11-28 00:01:15 -0500

    real webm done

--- server/video-processor.js
+++ server/video-processor.js
@@ -7,17 +7,24 @@ var url = require('url');
 require('date-utils');
 var videoFileExtension = '.webm';
 var blobs = [];
+var prevFilePath = '';
 function writeOrAppendData(data, fileName, ws) {
     var filePath = '../www/w/';
     if (!fs.existsSync(filePath)){
         fs.mkdirSync(filePath);
     }
-    var livePath = filePath+fileName;
+    var livePath = filePath+fileName+'/';
     if (!fs.existsSync(livePath)){
         var t_hms = new Date().getTime();
-        console.log(t_hms);
         fs.mkdirSync(livePath);
-
+        prevFilePath = livePath + t_hms + videoFileExtension;
+        fs.writeFileSync(prevFilePath, data);
+    }else{
+        //delete previous file
+        fs.unlinkSync(prevFilePath);
+        var t_hms = new Date().getTime();
+        prevFilePath = livePath + t_hms + videoFileExtension;
+        fs.writeFileSync(prevFilePath, data);
     }
 
     if (!fs.existsSync(filePath + fileName + videoFileExtension)) {
